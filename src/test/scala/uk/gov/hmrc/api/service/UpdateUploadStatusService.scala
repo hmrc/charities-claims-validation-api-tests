9e133524675a11514dc50466553cd10d42f1d729
KezzaTezza99 <63915795+KezzaTezza99@users.noreply.github.com>
2026-01-05 15:41:30 +0000

    Added more test cases to UpdateUploadSpec and some minor refactoring

--- src/test/scala/uk/gov/hmrc/api/service/UpdateUploadStatusService.scala
+++ src/test/scala/uk/gov/hmrc/api/service/UpdateUploadStatusService.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.api.service
 
 import play.api.libs.json.Json
@@ -11,12 +27,17 @@ import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 
 class UpdateUploadStatusService extends HttpClient {
-  val host: String = TestEnvironment.url("Charities Claims Validation")
-  val endpoint: String = "/claim-123/upload-results/ref-001"
+  val host: String     = TestEnvironment.url("Charities Claims Validation")
+  val endpoint: String = "upload-results"
 
-  def postAPayloadObject(payload: UpdateUploadStatusPayload, token: String): StandaloneWSResponse =
+  def postAPayloadObject(
+    claimId: String,
+    reference: String,
+    payload: UpdateUploadStatusPayload,
+    token: String
+  ): StandaloneWSResponse =
     Await.result(
-      mkRequest(host + endpoint)
+      mkRequest(s"$host/$claimId/$endpoint/$reference")
         .withHttpHeaders(
           "Authorization" -> s"Bearer $token"
         )
