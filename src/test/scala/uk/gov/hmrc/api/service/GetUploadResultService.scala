3556fdf66cf617c6b82aa192dc71a19ef90d8d08
KezzaTezza99 <63915795+KezzaTezza99@users.noreply.github.com>
2026-01-09 10:00:13 +0000

    Service for  DTR-2172 added

--- src/test/scala/uk/gov/hmrc/api/service/GetUploadResultService.scala
+++ src/test/scala/uk/gov/hmrc/api/service/GetUploadResultService.scala
@@ -1,5 +1,27 @@
 package uk.gov.hmrc.api.service
 
-class GetUploadResultService {
+import play.api.libs.ws.StandaloneWSResponse
+import uk.gov.hmrc.api.conf.TestEnvironment
+import uk.gov.hmrc.apitestrunner.http.HttpClient
 
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+
+class GetUploadResultService extends HttpClient {
+  val host: String     = TestEnvironment.url("Charities Claims Validation")
+  val endpoint: String = "upload-results"
+
+  def postAPayloadObject(
+    claimId: String,
+    reference: String,
+    token: String
+  ): StandaloneWSResponse =
+    Await.result(
+      mkRequest(s"$host/$claimId/$endpoint/$reference")
+        .withHttpHeaders(
+          "Authorization" -> s"Bearer $token"
+        )
+        .get(),
+      10.seconds
+    )
 }
