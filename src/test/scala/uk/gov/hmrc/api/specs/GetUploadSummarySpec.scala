055b8fa8f0fc2b116b7320c2d32d9a4941dd77f2
KezzaTezza99 <63915795+KezzaTezza99@users.noreply.github.com>
2026-01-22 13:26:20 +0000

    Refactored Upscan to accept paramaters that are needed for file validation, seems to be working

--- src/test/scala/uk/gov/hmrc/api/specs/GetUploadSummarySpec.scala
+++ src/test/scala/uk/gov/hmrc/api/specs/GetUploadSummarySpec.scala
@@ -1,7 +1,7 @@
 package uk.gov.hmrc.api.specs
 
 import uk.gov.hmrc.api.BaseSpec
-import uk.gov.hmrc.api.data.GetUploadSummaryData
+import uk.gov.hmrc.api.data.{CreateUploadTrackingData, CreateUpscanCallbackData, GetUploadSummaryData}
 import uk.gov.hmrc.api.data.globals.{FileStatus, ValidationType}
 import uk.gov.hmrc.api.helpers.UploadTestDataHelper
 import uk.gov.hmrc.api.service.GetUploadSummaryService
@@ -12,14 +12,25 @@ class GetUploadSummarySpec extends BaseSpec with UploadTestDataHelper {
   /** The E2E test for this will be simply upload a GiftAid claim and provide valid data then check the response is as
     * expected
     */
-  //TODO: Blocked until Upscan Refactor
-//  Feature("Charities - Get Upload Summary API - E2E") {
-//    Scenario("Starting a GiftAid claim and providing valid data to Upscan", E2ETest) {
-//      uploadTestData(authToken)
-//      Then("We provide upscan with a valid payload that has a reference to the valid spreadsheet value")
-//
-//    }
-//  }
+  Feature("Charities - Get Upload Summary API - E2E") {
+    Scenario("Starting a GiftAid claim and providing valid data to Upscan", E2ETest) {
+      uploadTestData(
+        authToken,
+        claimId = GetUploadSummaryData.getIndividualClaimID(ValidationType.OtherIncome),
+        validationType = ValidationType.OtherIncome
+      )
+      Then("We provide upscan with a valid payload that has a reference to the valid spreadsheet value")
+      createUpscanService.postSuccessfulPayloadObject(
+        GetUploadSummaryData.getIndividualClaimID(ValidationType.OtherIncome),
+        CreateUpscanCallbackData.getSuccessfulCreateUpscanCallbackPayloadWithReference(
+          reference = "Create-Upload-Tracking-ref",
+          downloadUrl = GetUploadSummaryData.getSuccessfulFileLocations(ValidationType.OtherIncome),
+          fileName = GetUploadSummaryData.getSuccessfulFileLocations(ValidationType.OtherIncome, false)
+        ),
+        authToken
+      )
+    }
+  }
 
   // TODO: Finish
   Feature("Charities - Get Upload Summary API - Testing all response variations") {
@@ -53,7 +64,11 @@ class GetUploadSummarySpec extends BaseSpec with UploadTestDataHelper {
       )
 
       Then("We check CommunityBuildings response")
-      checkCommonResponseBodies(communityBuildingsResponse, ValidationType.CommunityBuildings, FileStatus.AWAITING_UPLOAD)
+      checkCommonResponseBodies(
+        communityBuildingsResponse,
+        ValidationType.CommunityBuildings,
+        FileStatus.AWAITING_UPLOAD
+      )
 
       val connectedCharitiesResponse = getUploadSummaryService.getUploadSummaryResults(
         GetUploadSummaryData.getIndividualClaimID(ValidationType.ConnectedCharities),
@@ -61,7 +76,11 @@ class GetUploadSummarySpec extends BaseSpec with UploadTestDataHelper {
       )
 
       Then("We check ConnectedCharities response")
-      checkCommonResponseBodies(connectedCharitiesResponse, ValidationType.ConnectedCharities, FileStatus.AWAITING_UPLOAD)
+      checkCommonResponseBodies(
+        connectedCharitiesResponse,
+        ValidationType.ConnectedCharities,
+        FileStatus.AWAITING_UPLOAD
+      )
     }
 
     Scenario("Testing the four variations of 'validationType' where all claimIDs are associated to one user") {
